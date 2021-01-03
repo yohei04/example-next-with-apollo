087060759440f4e1547c3bc08a350d71c9d5ba5d
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-01-04 00:35:39 +0900

    change to ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
-import { ApolloProvider } from '@apollo/client'
-import { useApollo } from '../lib/apolloClient'
+import { ApolloProvider } from '@apollo/client';
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { useApollo } from '../lib/apolloClient';
 
-export default function App({ Component, pageProps }) {
-  const apolloClient = useApollo(pageProps)
+export default function App({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps);
 
   return (
     <ApolloProvider client={apolloClient}>
       <Component {...pageProps} />
     </ApolloProvider>
-  )
+  );
 }
