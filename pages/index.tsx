e80353d1a9ae5fb1c32782e48de18294a1d20af2
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-01-03 20:11:38 +0900

    change index.js to ts

---
+++ pages/index.tsx
@@ -0,0 +1,35 @@
+import App from '../components/App';
+import InfoBox from '../components/InfoBox';
+import Header from '../components/Header';
+import Submit from '../components/Submit';
+import PostList, {
+  ALL_POSTS_QUERY,
+  allPostsQueryVars,
+} from '../components/PostList';
+import { initializeApollo, addApolloState } from '../lib/apolloClient';
+import { GetStaticProps } from 'next';
+
+const IndexPage = () => (
+  <App>
+    <Header />
+    <InfoBox>ℹ️ This page shows how to use SSG with Apollo.</InfoBox>
+    <Submit />
+    <PostList />
+  </App>
+);
+
+export const getStaticProps: GetStaticProps = async () => {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: ALL_POSTS_QUERY,
+    variables: allPostsQueryVars,
+  });
+
+  return addApolloState(apolloClient, {
+    props: {},
+    revalidate: 1,
+  });
+};
+
+export default IndexPage;
